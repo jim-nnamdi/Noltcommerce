b752ed6958daf441347141248a637f5be574cbb4
jim-nnamdi <jimsamuel50@gmail.com>
2020-10-29 20:51:52 +0100

    get categories for homepage

--- resources/js/components/Front.vue
+++ resources/js/components/Front.vue
@@ -1,10 +1,44 @@
 <template>
     <div class="container mt-5 mb-5">
         <div class="row">
-            <h1>Noltcommerce</h1>
+            <div class="col-lg-3">
+                <h1>Nolt Catalog</h1>
+                <div class="list-group">
+                    <a
+                        class="list-group-item"
+                        v-for="category in categories"
+                        :key="category"
+                        >{{ category.name }}</a
+                    >
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script>
-export default {};
+export default {
+    data: function () {
+        return {
+            categories: [],
+            products: [],
+        };
+    },
+    mounted() {
+        this.loadCategories();
+        this.loadProducts();
+    },
+    methods: {
+        loadCategories: function () {
+            axios
+                .get("/api/v1/categories")
+                .then((response) => {
+                    this.categories = response.data.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        loadProducts: function () {},
+    },
+};
 </script>
